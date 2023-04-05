3d2f9d75d6cbd6b1196572b4d8b4f9e5b877100a
Volodymyr Hensorskyi <vovchukgensorskyy@gmail.com>
2023-04-05 22:06:10 +0300

    Implement base listener

--- common/src/index.ts
+++ common/src/index.ts
@@ -11,3 +11,10 @@ export * from './middlewares/errorHandler'
 export * from './middlewares/requestValidate'
 export * from './middlewares/userAuthorize'
 export * from './middlewares/userSet'
+
+//events
+export * from './events/baseListener'
+export * from './events/basePublisher'
+export * from './events/subjects'
+export * from './events/ticketCreatedEvent'
+export * from './events/ticketUpdatedEvent'
