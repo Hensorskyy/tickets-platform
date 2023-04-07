722cfd7647365be94f7297593954e24f9eef6c0b
Volodymyr Hensorskyi <vovchukgensorskyy@gmail.com>
2023-04-07 17:28:36 +0300

    add create order logic

--- orders/src/routes/apiRoutes.ts
+++ orders/src/routes/apiRoutes.ts
@@ -1,8 +1,13 @@
-import { NotAuthorizedError, NotFoundError, TicketData, requestValidate, userAuthorize, userSet } from "@vhticketing/common";
+import { BadRequestError, NotAuthorizedError, NotFoundError, OrderStatus, requestValidate, userAuthorize } from "@vhticketing/common";
 import express, { Request, Response } from "express";
 
+import { Order } from "../models/order";
+import { Ticket } from "../models/ticket";
 import { body } from "express-validator";
+import mongoose from "mongoose";
 import { natsWrapper } from "../natsWrapper";
+
+const EXPIRATION_SECONDS = 15 * 60
 
 const apiRouter = express.Router()
 
@@ -10,8 +15,38 @@ apiRouter.get('/orders', async (req: Request, res: Response) => {
   res.status(201).send()
 })
 
-apiRouter.post('/orders', async (req: Request, res: Response) => {
-  res.status(201).send()
+apiRouter.post('/orders', userAuthorize, [
+  body('ticketId')
+    .not()
+    .isEmpty()
+    .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+    .withMessage('TicketId must be provided')
+], requestValidate, async (req: Request, res: Response) => {
+  const { ticketId } = req.body
+
+  const ticket = await Ticket.findById(ticketId)
+  if (!ticket) {
+    throw new NotFoundError('The ticket does not exist')
+  }
+
+  const isReserved = await ticket.isReserved()
+  if (isReserved) {
+    throw new BadRequestError('The ticket is not available for purсhase')
+  }
+
+  const expiration = new Date()
+  expiration.setSeconds(expiration.getSeconds() + EXPIRATION_SECONDS)
+
+  const order = Order.build({
+    userId: req?.currentUser?.id as string,
+    status: OrderStatus.Created,
+    expiresAt: expiration,
+    ticket: ticket
+  })
+
+  await order.save()
+
+  res.status(201).send(order)
 })
 
 
