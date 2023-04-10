1a212fd082830c6a4f5b008cc3911269707320a5
Volodymyr Hensorskyi <vovchukgensorskyy@gmail.com>
2023-04-10 15:25:58 +0300

    add post order tests

--- orders/src/routes/__test__/apiRoutes.test.ts
+++ orders/src/routes/__test__/apiRoutes.test.ts
@@ -1,3 +1,6 @@
+import { Order } from '../../models/order'
+import { OrderStatus } from '@vhticketing/common'
+import { Ticket } from '../../models/ticket'
 import { app } from '../../app'
 import mongoose from 'mongoose'
 import request from 'supertest'
@@ -13,10 +16,28 @@ it('return an error if the ticket does not exist', async () => {
     .expect(404)
 })
 
-it('return an error if the ticket is already reserved', () => {
+it('return an error if the ticket is already reserved', async () => {
 
+  const ticket = Ticket.build({ title: 'footbal', price: 50 })
+  await ticket.save()
+
+  const order = Order.build({ userId: 'id', status: OrderStatus.Created, expiresAt: new Date(), ticket })
+  await order.save()
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', signin())
+    .send({ ticketId: ticket?.id })
+    .expect(400)
 })
 
-it('reserves a ticket', () => {
+it('reserves a ticket', async () => {
+  const ticket = Ticket.build({ title: 'footbal', price: 50 })
+  await ticket.save()
 
+  request(app)
+    .post('/api/orders')
+    .set('Cookie', signin())
+    .send({ ticketId: ticket?.id })
+    .expect(201)
 })
